3267d559a46eca5b05c1085da94056c1620e848f
hongsemy <semyeong.hong@mail.utoronto.ca>
2021-09-16 15:13:38 +0900

    <TST>: Bot test first commit

---
+++ uoft_news_bot.py
@@ -0,0 +1,14 @@
+import telegram
+
+my_token = '1966844637:AAGtjaF4bHILxIGBNLE5RXzDd0HG2arwneg'
+
+bot = telegram.Bot(token=my_token)
+
+updates = bot.getUpdates()
+
+for update in updates:
+    print(update.message)
+
+chat_id = bot.getUpdates()[-1].message.chat.id
+
+bot.sendMessage(chat_id = chat_id, text="Hello, I am a bot.")
